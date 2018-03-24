ff8b0867388721c16a51b4831172a94e5b40c115
xuzhudeng <xuzhudeng@qiyi.com>
2018-03-24 14:07:45 +0800

    verify Storage Permissions，resolve no access sdcard permission on Android version after 6.0
    others：
    1. delete togglePaused() in MainActivity.java while "java.lang.UnsatisfiedLinkError: No implementation found for void cn.jx.easyplayer.MainActivity.togglePaused() (tried Java_cn_jx_easyplayer_MainActivity_togglePaused and Java_cn_jx_easyplayer_MainActivity_togglePaused__)"

--- app/src/main/java/cn/jx/easyplayer/MainActivity.java
+++ app/src/main/java/cn/jx/easyplayer/MainActivity.java
@@ -1,13 +1,16 @@
 package cn.jx.easyplayer;
 
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
         final EasyVideoView videoView = (EasyVideoView) findViewById(R.id.easy_video_view);
         String folderurl = Environment.getExternalStorageDirectory().getPath();
         String inputurl = folderurl+"/jack.mp4";
@@ -75,10 +79,29 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE"};
+
+
+    public static void verifyStoragePermissions(Activity activity) {
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
-        togglePaused();
+//        togglePaused();
     }
 
 
@@ -118,7 +141,7 @@ public class MainActivity extends AppCompatActivity{
     }
 
 
-    public native void togglePaused();
+//    public native void togglePaused();
 
 
 
